c57613361a30a2e75ebb126c5702b5214e2789ab
radioraccoon <113745195+RadioRaccoon@users.noreply.github.com>
2022-10-11 17:48:22 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import chess
+
+
+
+'''
+AutoChess variant of chess. In this game mode, you'll control a chess engine in order to best your opponent. While you play, you'll select abilities to out-wit (or, troll) your opponent in order to win!
+
+'''
+
+
+for i in range(5):
+    if chess.outcome() is None:
